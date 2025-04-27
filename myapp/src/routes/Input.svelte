ed665818421638adffba76203b09ef311fef3f99
uladzimir <01myit@gmail.com>
2025-04-27 18:20:45 +0300

    onchange instead bind:value={value}.

--- myapp/src/routes/Input.svelte
+++ myapp/src/routes/Input.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     import '../styles/global.css'
-    let value = $state(null)
-
-    $effect(() => {
-        if(typeof value === 'number') fetch(`https://jsonplaceholder.typicode.com/posts/${value}`)
-                                            .then((response) => response.json())
-                                            .then((json) => console.log(json))
-    })
+    let value: null | number = $state(null)
+    function onChange(e: Event) {
+        const inputValue = Number((e.target as HTMLInputElement).value)
+        if(typeof inputValue === 'number') {
+            value = inputValue
+            fetch(`https://jsonplaceholder.typicode.com/posts/${inputValue}`)
+                .then((response) => response.json())
+                .then((json) => console.log(json))
+        }
+    }
+    $effect(() => {if(typeof value === 'number')console.log(`Do something with value: ${value}, for example`)})
 </script>
 <div>
-    <input type="number" name='post number' bind:value={value} placeholder="write post ID">
+    <input type="number" name='post number' {value} onchange={onChange} placeholder="write post ID">
 </div>
