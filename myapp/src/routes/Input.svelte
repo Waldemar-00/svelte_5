2369766958851017d4bee932cfed87e628a4e8f7
uladzimir <01myit@gmail.com>
2025-04-28 14:59:37 +0300

    Rines: effect.pre.

--- myapp/src/routes/Input.svelte
+++ myapp/src/routes/Input.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import { untrack } from 'svelte';
     import '../styles/global.css'
     let value: null | number = $state(null)
+    let answer: boolean = $state(false)
     function onChange(e: Event) {
-        const inputValue = Number((e.target as HTMLInputElement).value)
-        if(typeof inputValue === 'number') {
-            value = inputValue
-            fetch(`https://jsonplaceholder.typicode.com/posts/${inputValue}`)
-                .then((response) => response.json())
-                .then((json) => console.log(json))
-        }
+        value = Number((e.target as HTMLInputElement).value)
     }
-    $effect(() => {if(typeof value === 'number')console.log(`Do something with value: ${value}, for example`)})
+    $effect.pre(() => {
+        if(value){
+           untrack(() => answer = confirm('Request will be sending with new ID!'))
+            untrack(() => answer) && fetch(`https://jsonplaceholder.typicode.com/posts/${value}`)
+                        .then((response) => response.json())
+                        .then((json) => console.log(json))
+        }
+    })
+    $effect(() => {if(value && answer)console.log(`Do something with value: ${value}, for example`)})
 </script>
 <div>
     <input type="number" name='post number' {value} onchange={onChange} placeholder="write post ID">
