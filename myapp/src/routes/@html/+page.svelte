61baf2767911d6216fc5a179405918197b32c9fb
uladzimir <01myit@gmail.com>
2025-05-03 13:57:51 +0300

    @html. SanitizeHtml for cleaning dirty string which contains HTML.

---
+++ myapp/src/routes/@html/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import sanitizeHtml from 'sanitize-html'
+    const html = '<img src=x onerror=alert(1) /><b>hello</b>'
+    const cleanHtml = sanitizeHtml(html)
+</script>
+
+{@html cleanHtml}
