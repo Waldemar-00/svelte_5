e61dd65db86a794c3c1ae53b49b7049122389977
uladzimir <01myit@gmail.com>
2025-05-08 20:16:04 +0300

    svelte:document with onvisibilitychange handle event.

---
+++ myapp/src/routes/document/+page.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+    let visibility = $state('visible')
+    function visibilityChange(e: any) {
+        console.log(`Document is hidden: ${e.target.hidden.toString().toUpperCase()}`)
+    }
+
+</script>
+<div class=visibility>
+    <h1>Change visibility</h1>
+</div>
+<svelte:document onvisibilitychange={visibilityChange}/>
+
+<style>
+    .visibility {
+        width: 50vw;
+        height: 20vh;
+        background-color: yellow;
+        border: 2px dotted green;
+        cursor: pointer;
+    }
+</style>
