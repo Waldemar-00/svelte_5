0c1526f6bc5b58e1a4df44f8f0c53dca2bac4d68
uladzimir <01myit@gmail.com>
2025-05-08 20:38:25 +0300

    svelte:body and mouse events: onmouseenter and onmouseleave.

---
+++ myapp/src/routes/body/+page.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+    let visibility = $state('visible')
+    function handleMouseEnterOrLeave(e: any) {
+        if(e.type === 'mouseenter')console.log("ENTER")
+        else console.log("LEAVE")
+    }
+
+</script>
+<div class=body>
+    <h1>Mouse Enter and Leave</h1>
+</div>
+<svelte:body onmouseenter={handleMouseEnterOrLeave} onmouseleave={handleMouseEnterOrLeave}/>
+
+<style>
+    .body {
+        width: 50vw;
+        height: 20vh;
+        background-color: yellow;
+        border: 2px dotted green;
+        cursor: pointer;
+    }
+</style>
