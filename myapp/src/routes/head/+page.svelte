6b9b736ce196eec77a7b1d1a743061ea70fbba8c
uladzimir <01myit@gmail.com>
2025-05-08 20:49:35 +0300

    svelte:head. tags: meta and title.

---
+++ myapp/src/routes/head/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+
+</script>
+
+<svelte:head>
+    <title>Hello svelte:head!</title>
+    <meta name='description' content="This is where the description goes for SEO"/>
+</svelte:head>
+
+<h1>Svelte:head. Look at the head tag</h1>
