2d5b49ba815d62ccba3df5215bf6a0d594106fa9
uladzimir <01myit@gmail.com>
2025-05-08 19:22:25 +0300

    svelte:boundry with snippet failed(error, reset).

---
+++ myapp/src/routes/boundary/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // @ts-nocheck
+    import Mouse from './Mouse.svelte'
+    import '../../styles/global.css'
+</script>
+<svelte:boundary>
+    <Mouse/>
+    {#snippet failed(error, reset)}
+    <h2 style="color: red">{error.message}</h2>
+        <button onclick={reset}>Reset this OOps!</button>
+    {/snippet}
+</svelte:boundary>
---
+++ myapp/src/routes/boundary/Mouse.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import '../../styles/global.css'
+    let mouse = $state({y: 0, x: 0})
+    function mouseMove(e: any) {
+        mouse.y = e.clientY
+        mouse.x = e.clientX
+    }
+    function notClick() {
+        //@ts-ignore
+        mouse = null
+    }
+</script>
+
+<svelte:window
+    onmousemove={mouseMove}
+/>
+<h2>Y: {mouse.y}</h2>
+<h2>X: {mouse.x}</h2>
+<button onclick={notClick}>Don't click here</button>
