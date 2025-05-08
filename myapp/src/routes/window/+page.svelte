6fdf6f9981563511d077b944b8b92ebe52fd3564
uladzimir <01myit@gmail.com>
2025-05-08 19:36:24 +0300

    svelte:window

---
+++ myapp/src/routes/window/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import '../../styles/global.css'
+    function alertKey(e: any) {
+        alert(`Has been pressed ${e.key} key`)
+    }
+</script>
+<h1>Svelte:window</h1>
+<svelte:window onkeydown={alertKey}/>
