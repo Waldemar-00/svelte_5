006260acea753155fb88f2054f2d54b48cc819f4
uladzimir <01myit@gmail.com>
2025-05-08 20:58:54 +0300

    svelte:element with THIS.

---
+++ myapp/src/routes/element/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let tag = $state('h1')
+</script>
+
+
+<svelte:element this={tag}>
+    Svelte element with tag {tag}
+</svelte:element>
+
+<input type="text" bind:value={tag}>
