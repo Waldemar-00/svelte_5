b246499ad514c4667230be092018f87554666eed
uladzimir <01myit@gmail.com>
2025-05-10 13:23:24 +0300

    Context: setContext and getContext(from svelte). Using with state - like  reactive props.

---
+++ myapp/src/routes/context/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { setContext} from 'svelte'
+    import Child from './Child.svelte'
+    let commonState: any = $state({a: 10, message: 'Hello Context!'})
+    setContext('CommonContext', commonState )
+    const ForChildContext = {b: 700}
+    setContext('ForChildContext', ForChildContext )
+</script>
+<h1>Context from parent to child</h1>
+<h2>Parent state: {commonState.a}</h2>
+<hr>
+<h2>For Child Context: {ForChildContext.b}</h2>
+<Child/>
---
+++ myapp/src/routes/context/Child.svelte
@@ -0,0 +1,22 @@
+<script>
+    import '../../styles/global.css'
+    import {getContext} from 'svelte'
+    const CommonContext = getContext('CommonContext')
+    function changeStateA() {
+        CommonContext.a++
+    }
+    function changeStateB() {
+        childState.b++
+    }
+    const childContext = getContext('ForChildContext')
+    let childState = $state(childContext)
+</script>
+
+<h2>Child state: {CommonContext.a}</h2>
+<h2>Gritting: {CommonContext.message}</h2>
+
+<button onclick={changeStateA}>Change a in state</button>
+<hr>
+<h4>reactive context only for child</h4>
+<h2>{childState.b}</h2>
+<button onclick={changeStateB}>Change b in state</button>
